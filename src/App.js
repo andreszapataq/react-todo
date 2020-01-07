ad2ba67ca81adefd131648a58fa45af9b9b8ae60
andreszapataq <andreszapataq@gmail.com>
2020-01-06 19:51:10 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Footer from './components/Footer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Sacar el perro a cagar',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Cena con Monica',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Hacer código',
+        completed: false
+      }
+    ]
+  }
 
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <Footer />
+      </div>
+    );
+  }
+}
+  
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Footer by AZ</h4>
+            </div>
+        );
+    }
+}
+
+export default Footer;
