1cc803cf66772dd3705bcb7437560b8566e32053
andreszapataq <andreszapataq@gmail.com>
2020-01-08 23:18:09 -0500

    prueba state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 class App extends Component {
   state = {
+    numero: 4,
     todos: [
       {
         id: 1,
@@ -43,7 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} caca={this.state.numero} />
         <Footer />
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -30,10 +30,11 @@ export class TodoItem extends Component {
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
-                    { title }
+                    {/* <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '} */}
+                    <input type="checkbox" onChange={() => {this.props.markComplete(id)}} /> {' '}
+                    { this.props.popo }
                     <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
-                </p>    
+                </p>
             </div>
         )
     }
